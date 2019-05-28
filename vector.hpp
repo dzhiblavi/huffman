5ca9fb3a9beed90e9db949f8d874301158a662e5
dzhiblavi <dzhiblavi@gmail.com>
2019-05-28 20:45:53 +0300

    vector

--- vector.hpp
+++ vector.hpp
@@ -10,152 +10,208 @@
 #include <type_traits>
 #include <cstring>
 #include <algorithm>
+#include <memory>
 #include <iostream>
-
-template<typename T>
-T* _allocate_new_zone(T const* __restrict__ _src, size_t size, size_t alloc) {
-    auto alloc_data = static_cast<T *> (operator new(alloc * sizeof(T)));
-    std::copy(_src, _src + size, alloc_data);
-    return alloc_data;
-}
 
 /*
  * vector
  * small object
  * */
-template<typename T, size_t INIT_SO_SIZE_ = 6>
+template <typename T, size_t INIT_SO_SIZE_ = 6, typename Alloc = std::allocator<T>,
+        typename = typename std::enable_if_t<std::is_default_constructible_v<T>>>
 class vector {
 public:
-    using value_type = T;
-    using reference = T &;
-    using const_reference = T const &;
-    using pointer = T *;
-    using const_pointer = T const *;
+    typedef T                                           value_type;
+    typedef Alloc                                       allocator_type;
+    typedef std::allocator_traits<allocator_type>       allocator_traits_;
+    typedef typename allocator_traits_::size_type       size_type;
+    typedef typename allocator_traits_::difference_type difference_type;
+    typedef T&                                          reference;
+    typedef T const&                                    const_reference;
+    typedef typename allocator_traits_::pointer         pointer;
+    typedef typename allocator_traits_::const_pointer   const_pointer;
 
 private:
-    T small_[INIT_SO_SIZE_];
+    value_type small_[INIT_SO_SIZE_];
+    size_type size_ = 0;
+    size_type capacity_ = INIT_SO_SIZE_;
+
+    allocator_type alloc_;
     pointer data_ = small_;
 
-    size_t size_ = 0;
-    size_t capacity_ = INIT_SO_SIZE_;
+    pointer allocate_new_zone_(size_type cap_, size_type sz_, const_pointer src) {
+        pointer allocated_ = alloc_.allocate(cap_);
+        try {
+            std::copy(src, src + sz_, allocated_);
+        } catch (...) {
+            alloc_.deallocate(allocated_, cap_);
+            throw;
+        }
+        return allocated_;
+    }
 
-    void _clear() {
+    void destroy_() {
         std::destroy(data_, data_ + size_);
         if (data_ != small_) {
-            operator delete(data_);
+            alloc_.deallocate(data_, capacity_);
         }
     }
 
-    void _set_unique_large_data_(pointer __restrict__ allocated_, size_t new_capacity_) {
-        data_ = allocated_;
-        capacity_ = new_capacity_;
-    }
-
-    void _set_unique_small_data_() {
+    void set_small_() {
         data_ = small_;
         capacity_ = INIT_SO_SIZE_;
     }
 
-    void _push_back_short_path(const_reference x) {
-        new(data_ + size_) T(x);
+    void set_large_(pointer allocated_, size_type cap_) {
+        data_ = allocated_;
+        capacity_ = cap_;
     }
 
-    void _push_back_long_path(const_reference x) {
-        size_t new_capacity_ = capacity_ << 1;
-        pointer alloc_data_ = _allocate_new_zone(data_, size_, new_capacity_);
+    template <typename InputIt,
+            typename = typename std::enable_if_t<
+                    std::is_constructible_v<typename std::iterator_traits<InputIt>::reference>>>
+    vector(InputIt first, InputIt last, std::input_iterator_tag) {
+        while (first != last) {
+            push_back(*first++);
+        }
+    }
+
+    template <typename ForwardIt,
+            typename = typename std::enable_if_t<
+                    std::is_constructible_v<typename std::iterator_traits<ForwardIt>::reference>>>
+    vector(ForwardIt first, ForwardIt last, std::forward_iterator_tag) {
+        size_type dist = std::distance(first, last);
+        if (dist > INIT_SO_SIZE_) {
+            pointer allocated_ = alloc_.allocate(dist);
+            try {
+                std::copy(first, last, allocated_);
+            } catch (...) {
+                alloc_.deallocate(allocated_, dist);
+                throw;
+            }
+            set_large_(allocated_, dist);
+        } else {
+            std::copy(first, last, data_);
+        }
+        size_ = dist;
+    }
+
+    template <typename InputIt,
+            typename = typename std::enable_if_t<
+                    std::is_constructible_v<typename std::iterator_traits<InputIt>::reference>>>
+    void assign(InputIt first, InputIt last, std::input_iterator_tag) {
+        vector tmp;
+        while (first != last) {
+            tmp.push_back(*first++);
+        }
+        swap(tmp);
+    }
+
+    template <typename ForwardIt,
+            typename = typename std::enable_if_t<
+                    std::is_constructible_v<typename std::iterator_traits<ForwardIt>::reference>>>
+    void assign(ForwardIt first, ForwardIt last, std::forward_iterator_tag) {
+        size_type dist = std::distance(first, last);
+        if (dist > INIT_SO_SIZE_) {
+            pointer allocated_ = alloc_.allocate(dist);
+            try {
+                std::copy(first, last, allocated_);
+            } catch (...) {
+                alloc_.deallocate(allocated_, dist);
+                throw;
+            }
+            destroy_();
+            set_large_(allocated_, dist);
+        } else {
+            if (small()) {
+                destroy_();
+                std::copy(first, last, small_);
+            } else {
+                std::copy(first, last, small_);
+                destroy_();
+            }
+            set_small_();
+        }
+        size_ = dist;
+    }
+
+    template <typename Tp>
+    void push_back_slow_path_(Tp&& x) {
+        pointer allocated_ = allocate_new_zone_(capacity_ << 1, size_, data_);
         try {
-            new(alloc_data_ + size_) T(x);
+            new(allocated_ + size_) T(std::forward<Tp>(x));
         } catch (...) {
-            std::destroy(alloc_data_, alloc_data_ + size_);
-            operator delete(alloc_data_);
+            alloc_.deallocate(allocated_, capacity_ << 1);
             throw;
         }
-        _clear();
-        _set_unique_large_data_(alloc_data_, new_capacity_);
+        destroy_();
+        set_large_(allocated_, capacity_ << 1);
     }
 
-    void _resize_short_path(size_t new_size_) {
-        if (new_size_ > size_) {
-            std::uninitialized_fill(data_ + size_, data_ + new_size_, T());
-        } else if (new_size_ < size_) {
-            std::destroy(data_ + new_size_, data_ + size_);
-        }
-    }
-
-    void _resize_long_path(size_t new_size_) {
-        pointer alloc_data_ = _allocate_new_zone(data_, size_, new_size_);
+    template <typename... Args>
+    void emplace_back_slow_path_(Args&&... args) {
+        pointer allocated_ = allocate_new_zone_(capacity_ << 1, size_, data_);
         try {
-            std::uninitialized_fill(alloc_data_ + size_, alloc_data_ + new_size_, T());
+            new(allocated_ + size_) T(std::forward<Args>(args)...);
         } catch (...) {
-            std::destroy(alloc_data_, alloc_data_ + size_);
-            operator delete(alloc_data_);
+            alloc_.deallocate(allocated_, capacity_ << 1);
             throw;
         }
-        _clear();
-        _set_unique_large_data_(alloc_data_, new_size_);
+        destroy_();
+        set_large_(allocated_, capacity_ << 1);
     }
 
 public:
-    vector() = default;
+    vector() noexcept = default;
 
-    template<typename = typename std::enable_if<std::is_default_constructible_v<T>>::type>
-    explicit vector(size_t initial_size_) {
-        if (initial_size_ <= INIT_SO_SIZE_) {
-            std::uninitialized_fill(data_, data_ + initial_size_, T());
-        } else {
-            auto alloc_data_ = static_cast<pointer> (operator new(initial_size_ * sizeof(T)));
-            try {
-                std::uninitialized_fill(alloc_data_, alloc_data_ + initial_size_, T());
-            } catch (...) {
-                operator delete (alloc_data_);
-            }
-            _set_unique_large_data_(alloc_data_, initial_size_);
-        }
-        size_ = initial_size_;
-    }
-
-    template<typename ForwardIt>
-    vector(ForwardIt first, ForwardIt last) {
-        auto size = first - last;
-        reserve(size);
-        std::for_each(first, last, [this](auto const& x) {
-            push_back(x);
-        });
-    }
-
-    ~vector() noexcept {
-        _clear();
-    }
-
-    vector(vector &&rhs) {
-        swap(rhs);
-    }
-
-    vector(vector const &rhs) {
+    vector(vector const& rhs) {
         if (rhs.size_ <= INIT_SO_SIZE_) {
             std::copy(rhs.data_, rhs.data_ + rhs.size_, data_);
         } else {
-            pointer alloc_data = _allocate_new_zone(rhs.data_, rhs.size_, rhs.size_);
-            _set_unique_large_data_(alloc_data, rhs.size_);
+            pointer allocated_ = allocate_new_zone_(rhs.size_, rhs.size_, rhs.data_);
+            set_large_(allocated_, rhs.size_);
         }
         size_ = rhs.size_;
     }
 
-    vector& swap(vector &v) {
-        if (small() && v.small()) {
-            std::swap_ranges(data_, data_ + INIT_SO_SIZE_, v.data_);
-        } else if (small()) {
-            std::copy(small_, small_ + size_, v.small_);
-            std::swap(data_, v.data_);
-            capacity_ = v.capacity_;
-            v._set_unique_small_data_();
-        } else if (v.small()) {
-            v.swap(*this);
+    vector(vector&& rhs) {
+        swap(rhs);
+    }
+
+    explicit vector(size_type initial_size, const_reference x = T()) {
+        if (initial_size > INIT_SO_SIZE_) {
+            pointer allocated_ = alloc_.allocate(initial_size);
+            try {
+                std::uninitialized_fill(allocated_, allocated_ + initial_size, x);
+            } catch (...) {
+                alloc_.deallocate(allocated_, initial_size);
+                throw;
+            }
+            set_large_(allocated_, initial_size);
         } else {
-            std::swap(data_, v.data_);
-            std::swap(capacity_, v.capacity_);
+            std::uninitialized_fill(data_, data_ + initial_size, x);
         }
-        std::swap(size_, v.size_);
+        size_ = initial_size;
+    }
+
+    template <typename Iterator>
+    vector(Iterator first, Iterator last)
+            : vector(first, last, typename std::iterator_traits<Iterator>::iterator_category()) {}
+
+    ~vector() noexcept {
+        destroy_();
+    }
+
+    vector& operator=(vector const& rhs) {
+        if (rhs.size_ <= capacity_) {
+            destroy_();
+            std::copy(rhs.data_, rhs.data_ + rhs.size_, data_);
+        } else {
+            pointer allocated_ = allocate_new_zone_(rhs.capacity_, rhs.size_, rhs.data_);
+            destroy_();
+            set_large_(allocated_, rhs.capacity_);
+        }
+        size_ = rhs.size_;
         return *this;
     }
 
@@ -164,55 +220,71 @@ public:
         return *this;
     }
 
-    vector& operator=(vector const &rhs) {
-        if (rhs.size_ <= INIT_SO_SIZE_) {
-            std::copy(rhs.data_, rhs.data_ + rhs.size_, small_);
-            _clear();
-            _set_unique_small_data_();
+    vector& swap(vector& rhs) {
+        if (small() && rhs.small()) {
+            std::swap_ranges(data_, data_ + INIT_SO_SIZE_, rhs.data_);
+        } else if (small()) {
+            std::move(small_, small_ + size_, rhs.small_);
+            std::destroy(small_, small_ + size_);
+            set_large_(rhs.data_, rhs.capacity_);
+            rhs.set_small_();
+        } else if (rhs.small()) {
+            rhs.swap(*this);
         } else {
-            pointer alloc_data = _allocate_new_zone(rhs.data_, rhs.size_, rhs.size_);
-            _clear();
-            _set_unique_large_data_(alloc_data, rhs.size_);
+            std::swap(data_, rhs.data_);
+            std::swap(capacity_, rhs.capacity_);
         }
-        size_ = rhs.size_;
+        std::swap(size_, rhs.size_);
         return *this;
     }
 
-    void push_back(const_reference x) {
-        if (size_ < capacity_) {
-            _push_back_short_path(x);
+    void assign(size_type count, const_reference x) {
+        if (count <= capacity_) {
+            destroy_();
+            std::uninitialized_fill(data_, data_ + count, x);
         } else {
-            _push_back_long_path(x);
+            pointer allocated_ = alloc_.allocated(count);
+            try {
+                std::uninitialized_fill(allocated_, data_ + count, x);
+            } catch (...) {
+                alloc_.deallocate(allocated_);
+                throw;
+            }
+            destroy_();
+            set_large_(allocated_, count);
         }
-        ++size_;
+        size_ = count;
     }
 
-    void pop_back() noexcept {
-        --size_;
-        data_[size_].~T();
+    template <typename Iterator>
+    void assign(Iterator first, Iterator last) {
+        assign(first, last, typename std::iterator_traits<Iterator>::iterator_category());
     }
 
-    void resize(size_t newsize_) {
-        if (newsize_ <= capacity_) {
-            _resize_short_path(newsize_);
-        } else {
-            _resize_long_path(newsize_);
+    allocator_type get_allocator() const noexcept {
+        return alloc_;
+    }
+
+    reference at(size_t i) {
+        if (i >= size_) {
+            throw std::out_of_range("access index out of range");
         }
-        size_ = newsize_;
+        return data_[i];
     }
 
-    void reserve(size_t new_cap) {
-        if (new_cap < capacity_) {
-            return;
+    const_reference at(size_t i) const {
+        if (i >= size_) {
+            throw std::out_of_range("access index out of range");
         }
-        pointer alloc_data = _allocate_new_zone(data_, size_, new_cap);
-        _clear();
-        _set_unique_large_data_(alloc_data, new_cap);
+        return data_[i];
     }
 
-    void clear() {
-        std::destroy(data_, data_ + size_);
-        size_ = 0;
+    reference operator[](size_t i) noexcept {
+        return data_[i];
+    }
+
+    const_reference operator[](size_t i) const noexcept {
+        return data_[i];
     }
 
     reference front() noexcept {
@@ -223,20 +295,12 @@ public:
         return data_[0];
     }
 
-    reference back() noexcept {
+    reference back() {
         return data_[size_ - 1];
     }
 
     const_reference back() const noexcept {
         return data_[size_ - 1];
-    }
-
-    reference operator[](size_t i) {
-        return data_[i];
-    }
-
-    const_reference operator[](size_t i) const noexcept {
-        return data_[i];
     }
 
     pointer data() noexcept {
@@ -247,20 +311,104 @@ public:
         return data_;
     }
 
-    size_t size() const noexcept {
+    bool empty() const noexcept {
+        return size_ == 0;
+    }
+
+    size_type size() const noexcept {
         return size_;
     }
 
-    size_t capacity() const noexcept {
+    void reserve(size_type cap_) {
+        if (cap_ <= capacity_) {
+            return;
+        }
+        pointer allocated_ = allocate_new_zone_(cap_, size_, data_);
+        destroy_();
+        set_large_(allocated_, cap_);
+    }
+
+    size_type capacity() const noexcept {
         return capacity_;
     }
 
-    bool empty() const noexcept {
-        return !size_;
+    void shrink_to_fit() {
+        if (capacity_ == size_ || capacity_ == INIT_SO_SIZE_) {
+            return;
+        }
+        if (size_ <= INIT_SO_SIZE_) {
+            std::move(data_, data_ + size_, small_);
+            destroy_();
+            set_small_();
+        } else {
+            pointer allocated_ = allocate_new_zone_(size_, size_, data_);
+            destroy_();
+            set_large_(allocated_, size_);
+        }
     }
 
     bool small() const noexcept {
         return data_ == small_;
+    }
+
+    void clear() noexcept {
+        destroy_();
+        set_small_();
+        size_ = 0;
+    }
+
+    void pop_back() noexcept {
+        --size_;
+        std::destroy(data_ + size_, data_ + size_ + 1);
+    }
+
+    void push_back(const_reference x = T()) {
+        if (size_ < capacity_) {
+            new (data_ + size_) T(x);
+        } else {
+            push_back_slow_path_(x);
+        }
+        ++size_;
+    }
+
+    void push_back(value_type&& x) {
+        if (size_ < capacity_) {
+            new (data_ + size_) T(std::move(x));
+        } else {
+            push_back_slow_path_(std::move(x));
+        }
+        ++size_;
+    }
+
+    template <typename... Args>
+    void emplace_back(std::enable_if_t<std::is_constructible_v<value_type, Args>, Args>&&... args) {
+        if (size_ < capacity_) {
+            new(data_ + size_) T(std::forward<Args>(args)...);
+        } else {
+            emplace_back_slow_path_(std::forward<Args>(args)...);
+        }
+        ++size_;
+    }
+
+    void resize(size_type sz_) {
+        if (sz_ <= size_) {
+            std::destroy(data_ + sz_, data_ + size_);
+        } else {
+            if (sz_ <= capacity_) {
+                std::uninitialized_fill(data_ + size_, data_ + sz_, value_type());
+            } else {
+                pointer allocated_ = allocate_new_zone_(sz_, size_, data_);
+                try {
+                    std::uninitialized_fill(allocated_ + size_, allocated_ + sz_, T());
+                } catch (...) {
+                    alloc_.deallocate(allocated_, sz_);
+                    throw;
+                }
+                destroy_();
+                set_large_(allocated_, sz_);
+            }
+        }
+        size_ = sz_;
     }
 
     struct const_iterator : std::iterator<std::random_access_iterator_tag, value_type,
@@ -464,6 +612,7 @@ public:
     };
 
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
     using reverse_iterator = std::reverse_iterator<iterator>;
 
     const_iterator begin() const {
@@ -513,3 +662,30 @@ public:
 
 
 #endif /* vector.hpp */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
